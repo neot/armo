f804fd5211163cd571ff277245fadd489d5ca818
thierry forli <thierry.forli@gmail.com>
2014-03-04 14:17:17 +0100

    debug

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-var fs = require('fs');
-fs.writeFileSync("/var/lib/openshift/5315a46d5973ca221c0002b1/mynode.log", "running", "UTF-8");
 if(typeof process.env.OPENSHIFT_NODEJS_PORT === 'undefined'){
 	var port = 8080;
 }
@@ -19,6 +17,5 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
 	  res.writeHead(200);
 	    res.end('working');
-	    fs.writeFileSync("~/node.log", "res", "UTF-8");
 });
 server.listen(port, ip, function(){});
