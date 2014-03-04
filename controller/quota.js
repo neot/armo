080dc777306104b06491cbe5e8a5c798f744a574
thierry forli <thierry.forli@gmail.com>
2014-03-04 16:14:16 +0100

    idem

--- controller/quota.js
+++ controller/quota.js
@@ -1,11 +1,10 @@
 var dowork = function(cb){
 	var exec = require('child_process').exec, child;
 	child = exec('quota -w',
-			function (error, stdout, stderr) {
+			function(error, stdout, stderr){
 				cb(null, 'out');
 			}
 	);
-	 
 	child();
 };
 exports.dowork = dowork;
