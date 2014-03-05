bfe754bbf20134a603023c9a618191fec487850a
thierry forli <thierry.forli@gmail.com>
2014-03-05 18:23:43 +0100

    idem

--- controller/git.js
+++ controller/git.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec,
     child;
 var util = require('util');
 var dowork = function(cb){
-  fs.readFile('/var/lib/openshift/5315a46d5973ca221c0002b1/git/'+process.env.OPENSHIFT_APP_NAME+'.git/refs/heads/master', function(err, data){
+  fs.readFile('~/git/'+process.env.OPENSHIFT_APP_NAME+'.git/refs/heads/master', function(err, data){
     if(err){
       cb(err.message);
     }
