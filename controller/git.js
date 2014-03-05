4de29c9e818ae103f0b15c57988e9a942f7b327d
thierry forli <thierry.forli@gmail.com>
2014-03-05 15:20:22 +0100

    idem

--- controller/git.js
+++ controller/git.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec,
 var util = require('util');
 
 var dowork = function(cb){
-  exec("quota -w | sed -n 3p | cut -d ' ' -f 5",
+  exec("cat ~/git/*.git/refs/heads/master",
       function(error, stdout, stderr){
           cb(stdout);
       });
