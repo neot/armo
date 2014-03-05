54fabb852f324eb8feff6196101ddc4a22819b3a
thierry forli <thierry.forli@gmail.com>
2014-03-05 14:45:42 +0100

    idemœ:wq

--- controller/quota.js
+++ controller/quota.js
@@ -13,7 +13,7 @@ var dowork = function(cb){
     return util.format("%s:%s:%s", quota, ps, oo);
   }
 
-  exec("quota -w | sed -n 3p | cut -d ' ' -f 2",
+  exec("quota -w | sed -n 3p | cut -d ' ' -f 5",
       function(error, stdout, stderr){
         quota = stdout;
         count++;
