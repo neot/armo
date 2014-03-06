a496698e45c3679835eec2e7c336d97b3cb0d777
thierry forli <thierry.forli@gmail.com>
2014-03-06 11:04:15 +0100

    itest auth

--- server.js
+++ server.js
@@ -20,42 +20,48 @@ var quota = require('./controller/quota');
 var request = require('./controller/request');
 var git = require('./controller/git');
 var util = require('util');
+var basicAuth = require('./lib/basic-auth.js');
 
 var server = http.createServer(function(req, res) {
-	var page = url.parse(req.url).pathname;
-  var msg='';
-	if(page == '/'){
-		msg = home.dowork();
-		res.writeHead(200);
-    res.end(msg);
-	}
-	else if(page == '/quota'){
-		quota.dowork(
-      function(out){
-        msg=out;
-        res.writeHead(200);
-        res.end(msg);
-      });
-	}
-	else if(page.match('/request/.*')){
-		request.dowork(page.substr(page.lastIndexOf('/')+1), function(out){
-      res.writeHead(200);
-      msg = out;
+  basicAuth(req, function(){
+    var page = url.parse(req.url).pathname;
+    var msg='';
+    if(page == '/'){
+		  msg = home.dowork();
+		  res.writeHead(200);
       res.end(msg);
-    });
-	}
-	else if(page == '/git'){
-    git.dowork(
+    }
+    else if(page == '/quota'){
+		  quota.dowork(
         function(out){
           msg=out;
           res.writeHead(200);
           res.end(msg);
         });
-	}
-	else{
-		res.writeHead(404);
-		msg = '404';
-    res.end(msg);
-	}
+    }
+    else if(page.match('/request/.*')){
+		  request.dowork(page.substr(page.lastIndexOf('/')+1), function(out){
+        res.writeHead(200);
+        msg = out;
+        res.end(msg);
+      });
+    }
+    else if(page == '/git'){
+      git.dowork(
+        function(out){
+          msg=out;
+          res.writeHead(200);
+          res.end(msg);
+        });
+    }
+    else{
+		  res.writeHead(404);
+		  msg = '404';
+      res.end(msg);
+    }
+  }, function(code, h1, h2){
+    res.statusCode = 401;
+    res.setHeader(h1, h2);
+  });
 });
 server.listen(port, ip, function(){});
