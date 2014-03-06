d4791844cc2f6d43c0a5803132dc69e59638800a
thierry forli <thierry.forli@gmail.com>
2014-03-06 10:29:15 +0100

    idem

--- controller/git.js
+++ controller/git.js
@@ -1,17 +1,13 @@
 var fs = require('fs');
 var util = require('util');
 var dowork = function(cb){
-  fs.readFile(process.env.HOME+'git/'+process.env.OPENSHIFT_APP_NAME+'.git/refs/heads/master', function(err, data){
+  fs.readFile(process.env.HOME+'git/'+process.env.OPENSHIFT_APP_NAME+'.git/refs/heads/master', {encoding:'utf-8'}, function(err, data){
     if(err){
       cb(err.message);
     }
     else{
       d=data;
-      //cb(util.format('{ "master": "%s" }"', d));
-      if(data.contains('\n'))
-      {
-        data.replace('\n', '');
-      }
+      cb(util.format('{ "master": "%s" }"', d.replace('\n', '')));
     }
   });
 };
