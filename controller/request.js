5489a26bc3a7704f7b24baa2f94c223ad2ec7ec4
thierry <thierry@oenone>
2014-03-11 16:03:42 +0100

    idem

--- controller/request.js
+++ controller/request.js
@@ -3,7 +3,7 @@ var require = require('request');
 var dowork = function(site, cb){
   request(site, function (error, response, body) {
     if(error) {
-      return cb(error.message();
+      return cb(error.message());
     }
     cb(null, response.statusCode, body);
   });
