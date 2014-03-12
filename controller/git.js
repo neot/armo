f92e5b85b22c537608d08e130d480b7ddbdd6bbb
thierry <thierry@oenone>
2014-03-12 11:32:07 +0100

    git bug fix

--- controller/git.js
+++ controller/git.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 var dowork = function(cb){
-  var file = path.join(process.env.HOME,'git/',process.env.OPENSHIFT_APP_NAME,'.git/refs/heads/master');
+  var file = path.join(process.env.HOME,'git/',process.env.OPENSHIFT_APP_NAME+'.git/refs/heads/master');
   fs.readFile(file, {encoding:'utf-8'}, function(err, data){
     if(err){
       return cb(err);
