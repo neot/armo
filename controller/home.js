006ef90144b6c08b269e5f84d742e928e3bf180a
thierry <thierry@oenone>
2014-03-13 15:14:22 +0100

    template system working

--- controller/home.js
+++ controller/home.js
@@ -1,39 +1,51 @@
 var fs = require('fs');
 var request = require('request');
+var async = require('async');
 
 var getPage = function (cb){
-  fs.readFile("./public/index.html",  {encoding:'utf-8'}, function(err, data){
+  fs.readFile("./public/index.html", {encoding:'utf-8'}, function(err, data){
     if(err){
       return cb(err);
     }
-    var appUrl= process.env.OPENSHIFT_APP_DNS || "127.0.0.1";
-    var quotaRes = "";
-    var gitRes ="";
-    var requestRes="";
-    request("http://"+appUrl+"/"+"quota", function (error, response, body) {//quota
-      if(error) {
-        quotaRes=error.message;
+    var appUrl= process.env.OPENSHIFT_APP_DNS || "127.0.0.1:8080";
+    async.parallel([function(callb){
+      request("http://"+appUrl+"/"+"quota", function (error, response, body) {//quota
+        if(error) {
+          return callb(error);
+        }
+        callb(null, body);
+      });
+    }, function(callb){
+      request("http://"+appUrl+"/"+"git", function (error, response, body) {//git
+        if(error) {
+          callb(error);
+        }
+        callb(null, body);
+      });
+    }, function(callb){
+      request("http://"+appUrl+"/"+"request", function (error, response, body) {//request
+        if(error) {
+          callb(error);
+        }
+        callb(body);
+      });
+    }], function(err, results){
+      if(err){
+        return cb(err);
       }
-      quotaRes=body;
-    });
-    request("http://"+appUrl+"/"+"git", function (error, response, body) {//git
-      if(error) {
-        gitRes=error.message;
+      var str=data;
+      while(str.indexOf('$appUrl')!=-1){
+        str = str.replace('$appUrl', appUrl);
       }
-      gitRes=body;
-    });
-    request("http://"+appUrl+"/"+"request", function (error, response, body) {//request
-      if(error) {
-        requestRes=error.message;
+      while(str.indexOf("$quota")!=-1){
+        str = str.replace("$quota", results[0]);
       }
-      requestRes=body;
+      while(str.indexOf("$git")!=-1){
+        str = str.replace("$git", results[1]);
+      }
+      console.log(results[1]);
+      cb(null, str);
     });
-    
-    var str = data.replace('$appUrl', appUrl);
-    str = str.replace("$quota", quotaRes);
-    str = str.replace("$git", gitRes);
-    console.log(appUrl);
-    cb(null, str);
   });
 }
 
